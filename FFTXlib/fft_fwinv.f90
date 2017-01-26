e61c9e4c51275b2cf8a7930a11b9dda9bb86596f
Filippo Spiga <spiga.filippo@gmail.com>
2017-01-26 19:59:33 +0000

    Fixing FFTXlib... still not compile

--- FFTXlib/fft_fwinv.f90
+++ FFTXlib/fft_fwinv.f90
@@ -302,3 +302,219 @@ SUBROUTINE invfft_b( f, dfft, ia )
   RETURN
 END SUBROUTINE invfft_b
 !=---------------------------------------------------------------------------=!
+!
+!
+#ifdef USE_CUDA
+SUBROUTINE invfft_x_gpu( grid_type, f, dfft, dtgs, howmany )
+  !! Compute G-space to R-space for a specific grid type
+  !! 
+  !! **grid_type = 'Dense'** : 
+  !!   inverse fourier transform of potentials and charge density f
+  !!   on the dense grid . On output, f is overwritten
+  !! 
+  !! **grid_type = 'Smooth'** :
+  !!   inverse fourier transform of  potentials and charge density f
+  !!   on the smooth grid . On output, f is overwritten
+  !! 
+  !! **grid_type = 'Wave'** :
+  !!   inverse fourier transform of  wave functions f
+  !!   on the smooth grid . On output, f is overwritten
+  !! 
+  !! **grid_type = 'Custom'** : 
+  !!   inverse fourier transform of potentials and charge density f
+  !!   on a custom grid. On output, f is overwritten
+  !! 
+  !! **grid_type = 'CustomWave'** :
+  !!   inverse fourier transform of  wave functions f
+  !!   on a custom grid. On output, f is overwritten
+  !! 
+  !! **dfft = FFT descriptor**, IMPORTANT NOTICE: grid is specified only by dfft.
+  !!   No check is performed on the correspondence between dfft and grid_type.
+  !!   grid_type is now used only to distinguish cases 'Wave' / 'CustomWave' 
+  !!   from all other cases
+  
+  USE fft_scalar,    ONLY: cfft3d, cfft3ds
+  USE fft_smallbox,  ONLY: cft_b, cft_b_omp
+  USE fft_parallel,  ONLY: tg_cft3s
+  USE fft_types,     ONLY: fft_type_descriptor
+  USE task_groups,   ONLY: task_groups_descriptor
+  USE fft_param,     ONLY: DP
+
+  IMPLICIT NONE
+
+  TYPE(fft_type_descriptor), INTENT(IN) :: dfft
+  CHARACTER(LEN=*), INTENT(IN) :: grid_type
+  COMPLEX(DP), DEVICE :: f(:)
+  TYPE(task_groups_descriptor), OPTIONAL, INTENT(IN) :: dtgs
+  INTEGER, OPTIONAL, INTENT(IN) :: howmany
+  INTEGER :: howmany_ = 1
+
+  IF(PRESENT(howmany) ) THEN
+     howmany_ = howmany
+  END IF
+  !
+  IF( grid_type == 'Dense' ) THEN
+     CALL start_clock( 'fft' )
+  ELSE IF( grid_type == 'Smooth' ) THEN
+     CALL start_clock( 'ffts' )
+  ELSE IF( grid_type == 'Wave' ) THEN
+     CALL start_clock('fftw')
+  ELSE IF( grid_type == 'Custom' ) THEN
+     CALL start_clock('fftc')
+  ELSE IF( grid_type == 'CustomWave' ) THEN
+     CALL start_clock('fftcw')
+  ELSE 
+     CALL fftx_error__( ' invfft ', ' unknown grid: '//grid_type , 1 )
+  END IF
+
+  IF( dfft%lpara ) THEN
+
+     IF( howmany_ /= 1 ) THEN
+        CALL fftx_error__( ' invfft ', ' howmany not yet implemented for parallel driver ', 1 )
+     END IF
+     
+     IF( grid_type == 'Dense' .OR. grid_type == 'Smooth' .OR. &
+          grid_type == 'Custom' ) THEN
+        CALL tg_cft3s( f, dfft, 1 )
+     ELSE IF( grid_type == 'Wave' .OR. grid_type == 'CustomWave' ) THEN
+        CALL tg_cft3s( f, dfft, 2, dtgs )
+     END IF
+
+  ELSE
+
+     IF( grid_type == 'Dense' .OR. grid_type == 'Smooth' .OR. &
+         grid_type == 'Custom' ) THEN
+        CALL cfft3d( f, dfft%nr1, dfft%nr2, dfft%nr3, &
+                        dfft%nr1x,dfft%nr2x,dfft%nr3x, howmany_ , 1)
+     ELSE IF( grid_type == 'Wave' .OR. grid_type == 'CustomWave' ) THEN
+        CALL cfft3ds( f, dfft%nr1, dfft%nr2, dfft%nr3, &
+                         dfft%nr1x,dfft%nr2x,dfft%nr3x, howmany_ , 1, &
+                         dfft%isind, dfft%iplw )
+     END IF
+
+  END IF
+
+  IF( grid_type == 'Dense' ) THEN
+     CALL stop_clock( 'fft' )
+  ELSE IF( grid_type == 'Smooth' ) THEN
+     CALL stop_clock( 'ffts' )
+  ELSE IF( grid_type == 'Wave' ) THEN
+     CALL stop_clock('fftw')
+  ELSE IF( grid_type == 'Custom' ) THEN
+     CALL stop_clock('fftc')
+  ELSE IF( grid_type == 'CustomWave' ) THEN
+     CALL stop_clock('fftcw')
+  END IF
+
+  RETURN
+
+END SUBROUTINE invfft_x_gpu
+#endif
+!=---------------------------------------------------------------------------=!
+!
+!=---------------------------------------------------------------------------=!
+#ifdef USE_CUDA
+SUBROUTINE fwfft_x_gpu( grid_type, f, dfft, dtgs, howmany )
+  !! Compute R-space to G-space for a specific grid type
+  !! 
+  !! **grid_type = 'Dense'**
+  !!   forward fourier transform of potentials and charge density f
+  !!   on the dense grid . On output, f is overwritten
+  !! 
+  !! **grid_type = 'Smooth'**
+  !!   forward fourier transform of potentials and charge density f
+  !!   on the smooth grid . On output, f is overwritten
+  !! 
+  !! **grid_type = 'Wave'**
+  !!   forward fourier transform of  wave functions f
+  !!   on the smooth grid . On output, f is overwritten
+  !! 
+  !! **grid_type = 'Custom'**
+  !!   forward fourier transform of potentials and charge density f
+  !!   on a custom grid . On output, f is overwritten
+  !! 
+  !! **grid_type = 'CustomWave'**
+  !!   forward fourier transform of  wave functions
+  !!   on a custom grid . On output, f is overwritten
+  !! 
+  !! **dfft = FFT descriptor**, IMPORTANT NOTICE: grid is specified only by dfft.
+  !!   No check is performed on the correspondence between dfft and grid_type.
+  !!   grid_type is now used only to distinguish cases 'Wave' / 'CustomWave' 
+  !!   from all other cases
+  
+  USE fft_scalar,    ONLY: cfft3d, cfft3ds
+  USE fft_parallel,  ONLY: tg_cft3s
+  USE fft_types,     ONLY: fft_type_descriptor
+  USE task_groups,   ONLY: task_groups_descriptor
+  USE fft_param,     ONLY: DP
+
+  IMPLICIT NONE
+
+  TYPE(fft_type_descriptor), INTENT(IN) :: dfft
+  CHARACTER(LEN=*), INTENT(IN) :: grid_type
+  COMPLEX(DP), DEVICE :: f(:)
+  TYPE(task_groups_descriptor), OPTIONAL, INTENT(IN) :: dtgs
+  INTEGER, OPTIONAL, INTENT(IN) :: howmany
+  INTEGER :: howmany_ = 1
+
+  IF(PRESENT(howmany) ) THEN
+     howmany_ = howmany
+  END IF
+
+  IF( grid_type == 'Dense' ) THEN
+     CALL start_clock( 'fft' )
+  ELSE IF( grid_type == 'Smooth' ) THEN
+     CALL start_clock( 'ffts' )
+  ELSE IF( grid_type == 'Wave' ) THEN
+     CALL start_clock( 'fftw' )
+  ELSE IF( grid_type == 'Custom' ) THEN
+     CALL start_clock('fftc')
+  ELSE IF( grid_type == 'CustomWave' ) THEN
+     CALL start_clock('fftcw')
+  ELSE
+     CALL fftx_error__( ' fwfft ', ' unknown grid: '//grid_type , 1 )
+  END IF
+
+  IF( dfft%lpara ) THEN
+
+     IF( howmany_ /= 1 ) THEN
+        CALL fftx_error__( ' fwfft ', ' howmany not yet implemented for parallel driver ', 1 )
+     END IF
+     
+     IF( grid_type == 'Dense' .OR. grid_type == 'Smooth' .OR. &
+         grid_type == 'Custom' ) THEN
+        CALL tg_cft3s(f,dfft,-1)
+     ELSE IF( grid_type == 'Wave' .OR. grid_type == 'CustomWave' ) THEN
+        CALL tg_cft3s(f,dfft,-2, dtgs )
+     END IF
+
+  ELSE
+
+     IF( grid_type == 'Dense' .OR. grid_type == 'Smooth' .OR. &
+         grid_type == 'Custom' ) THEN
+        CALL cfft3d( f, dfft%nr1, dfft%nr2, dfft%nr3, &
+                        dfft%nr1x,dfft%nr2x,dfft%nr3x, howmany_ , -1)
+     ELSE IF( grid_type == 'Wave' .OR. grid_type == 'CustomWave' ) THEN
+        CALL cfft3ds( f, dfft%nr1, dfft%nr2, dfft%nr3, &
+                         dfft%nr1x,dfft%nr2x,dfft%nr3x, howmany_ , -1, &
+                         dfft%isind, dfft%iplw )
+     END IF
+
+  END IF
+
+  IF( grid_type == 'Dense' ) THEN
+     CALL stop_clock( 'fft' )
+  ELSE IF( grid_type == 'Smooth' ) THEN
+     CALL stop_clock( 'ffts' )
+  ELSE IF( grid_type == 'Wave' ) THEN
+     CALL stop_clock( 'fftw' )
+  ELSE IF( grid_type == 'Custom' ) THEN
+     CALL stop_clock('fftc')
+  ELSE IF( grid_type == 'CustomWave' ) THEN
+     CALL stop_clock('fftcw')
+  END IF
+  
+  RETURN
+  !
+END SUBROUTINE fwfft_x_gpu
+#endif
