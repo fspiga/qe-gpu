24e254876a79ca3c3d28dde8a38959928e03174b
Filippo Spiga <spiga.filippo@gmail.com>
2017-07-22 12:45:11 +0100

    Update Modules to v6.1

--- Modules/funct.f90
+++ Modules/funct.f90
@@ -425,10 +425,12 @@ CONTAINS
     ! special case : HCTH
     else if ('HCTH'.EQ. TRIM(dftout)) then
        dft_defined = set_dft_values(0,0,5,5,0,0)
+       call errore('set_dft_from_name','HCTH yields suspicious results',1)
               
     ! special case : OLYP = OPTX + LYP
     else if ('OLYP'.EQ. TRIM(dftout)) then
        dft_defined = set_dft_values(0,3,6,3,0,0)
+       call errore('set_dft_from_name','OLYP yields suspicious results',1)
        
     else if ('WC' .EQ. TRIM(dftout) ) then
     ! special case : Wu-Cohen
@@ -1035,9 +1037,9 @@ CONTAINS
         shortname = 'VDW-DF2-C09'
      else if (iexch==1.and.icorr==4.and.igcx==26.and.igcc==0) then
         shortname = 'VDW-DF2-B86R'
-     else if ( inlc==3) then
-        shortname = 'RVV10'
      end if
+  else if ( inlc==3) then
+     shortname = 'RVV10'
   end if
   !
   get_dft_short = shortname
@@ -1644,14 +1646,18 @@ subroutine gcx_spin (rhoup, rhodw, grhoup2, grhodw2, &
      sx = 0.5_DP * (sxup + sxdw)
      v2xup = 2.0_DP * v2xup
      v2xdw = 2.0_DP * v2xdw
-  elseif (igcx == 3 .or. igcx == 4 .or. igcx == 8 .or. &
-          igcx == 10 .or. igcx == 12 .or. igcx == 20 .or. igcx == 25) then
+  elseif (igcx == 3 .or. igcx == 4 .or. igcx == 8 .or. igcx ==10 .or. &
+          igcx ==12 .or. igcx ==20 .or. igcx ==23 .or. igcx ==24 .or. igcx == 25) then
      ! igcx=3: PBE, igcx=4: revised PBE, igcx=8: PBE0, igcx=10: PBEsol
-     ! igcx=12: HSE,  igcx=20: gau-pbe, igcx=25: ev93
+     ! igcx=12: HSE,  igcx=20: gau-pbe, igcx=23: obk8, igcx=24: ob86, igcx=25: ev93
      if (igcx == 4) then
         iflag = 2
      elseif (igcx == 10) then
         iflag = 3
+     elseif (igcx == 23) then
+        iflag = 5
+     elseif (igcx == 24) then
+        iflag = 6
      elseif (igcx == 25) then
         iflag = 7
      else
